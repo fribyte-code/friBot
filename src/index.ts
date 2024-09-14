bca0e4718b4cc0db9bb54c92388bda1aad9f2994
Simen Strømsnes <simen@simsine.no>
2024-09-14 11:29:59 +0200

    development

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ import { startCronTasks } from "./cron";
 const client = createMattermostClient();
 
 if (process.env.NODE_ENV === "production") startCronTasks(client);
+
+console.log(process.env.NODE_ENV)
